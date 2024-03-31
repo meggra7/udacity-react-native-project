505f6a51cb9cc64f78ffd24dc8af6ceae8ec2536
Meghan Dunham <meghan.g.dunham@gmail.com>
2024-03-31 09:39:10 -0400

    Polish Welcome screen layout and add button to clear customer data

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -2,23 +2,45 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Text, View } from "react-native";
 import { appStyles } from "../styles/main";
-import { PrimaryButton } from "../components/buttons";
+import { PrimaryButton, SecondaryButton } from "../components/buttons";
 import { Screen } from "../constants";
 
 export const Welcome: React.FC = () => {
   const { navigate } = useNavigation();
 
   return (
-    <View style={appStyles.container}>
-      <Text>Welcome Page</Text>
-      <PrimaryButton
-        text="Customers by region"
-        onPress={() => navigate(Screen.RegionList)}
-      />
-      <PrimaryButton
-        text="Add new customer"
-        onPress={() => navigate(Screen.AddCustomer)}
-      />
+    <View
+      style={{
+        ...appStyles.container,
+        justifyContent: "space-between",
+        padding: 24,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ marginBottom: 8 }}>
+          Select an option below to continue
+        </Text>
+        <PrimaryButton
+          text="View customers by region"
+          onPress={() => navigate(Screen.RegionList)}
+        />
+        <PrimaryButton
+          text="Add new customer"
+          onPress={() => navigate(Screen.AddCustomer)}
+        />
+      </View>
+
+      <SecondaryButton
+        text="Clear all customer data"
+        onPress={() => console.log("Clearing all customer data")}
+      ></SecondaryButton>
     </View>
   );
 };
